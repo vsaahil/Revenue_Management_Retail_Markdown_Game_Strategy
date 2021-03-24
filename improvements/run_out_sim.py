9d35c59954800841f3971df0f713236ca0ecef9e
Marek7869 <65094572+Marek7869@users.noreply.github.com>
2021-03-23 23:50:59 -0400

    Implementation of class solutions
    Coded the linear optimization and run out methods with LP re-optimization at every week 1 demand. Much better results, as suggested by the prof in class. RESULTS.txt contains the results for 1000 iterations of each.

---
+++ improvements/run_out_sim.py
@@ -0,0 +1,86 @@
+from selenium import webdriver
+from re import sub
+import numpy as np
+import statsmodels.stats.api as sms
+import pulp as plp
+from pulp import *
+
+# opens the browser for scraping
+browser=webdriver.Chrome(executable_path=r'C:\Users\Administrator\Documents\chromedriver.exe')
+browser.get("http://www.randhawa.us/games/retailer/nyu.html")
+
+# finds the buttons to click
+maintain = browser.find_element_by_id("maintainButton")
+dc_10 = browser.find_element_by_id("tenButton")
+dc_20 = browser.find_element_by_id("twentyButton")
+dc_40 = browser.find_element_by_id("fortyButton")
+restart = browser.find_element_by_class_name("button")
+
+scores = []
+
+# function to return LP optimized combo, minimizing the absolute difference between total demand and units available
+
+def get_weeks(sales):
+    prob = LpProblem("Run_Out_Rate")
+    prob.sense = LpMinimize
+    d_at_54 = sales*1.31
+    d_at_48 = sales*1.73
+    d_at_36 = sales*2.81
+    coeff = [sales,d_at_54,d_at_48,d_at_36]
+    units_available = 2000 - sales
+    variables = ["weeks_60","weeks_54","weeks_48","weeks_36"]
+    x_vars = {i:
+             LpVariable(name = variables[i], lowBound = 0, upBound=14, cat=LpInteger)
+             for i in range(len(variables))}
+    # minimize absolute deviation z (because needs to be linear)
+    z = LpVariable('z', cat='Continuous')
+    # total weeks need to == 14
+    prob += lpSum(x_vars) == 14
+    # linearization
+    prob += lpSum(x_vars[i] * coeff[i] for i in range(len(variables))) - units_available <= z
+    prob += -(lpSum(x_vars[i] * coeff[i] for i in range(len(variables))) - units_available) <= z
+    # objective and solve
+    prob.setObjective(lpSum(z))
+    prob.solve()
+    values = []
+    for v in prob.variables():
+        values.append(v.varValue)
+    return values[:4]
+
+# test = get_weeks(56)
+
+# order is 36, 48, 56, 60
+
+
+# Simulation
+
+for i in range(1000):
+    for tr in browser.find_elements_by_xpath('//table[@id="result-table"]//tr'):
+        tds = tr.find_elements_by_tag_name('td')
+    sales = int(tds[-2].text)
+    how_fun = get_weeks(sales)
+    for j in range(int(how_fun[-1])):
+        maintain.click()
+    if how_fun[-2] > 0:
+        dc_10.click()
+        for x in range(int(how_fun[-2]-1)):
+            maintain.click()
+    if how_fun[-3] > 0:
+        dc_20.click()
+        for p in range(int(how_fun[-3]-1)):
+            maintain.click()
+    if how_fun[-4] > 0:
+        dc_40.click()
+    revenue = int(sub(r'[^\d.]', '', browser.find_element_by_id("rev").text))
+    perfect = int(sub(r'[^\d.]', '', browser.find_element_by_id("perfect").text))
+    scores.append(1 - revenue/perfect)
+    restart.click()
+
+# print results
+
+print(np.mean(scores))
+print(np.min(scores))
+print(np.max(scores))
+print(np.std(scores, ddof=1))
+print(sms.DescrStatsW(scores).tconfint_mean())
+
